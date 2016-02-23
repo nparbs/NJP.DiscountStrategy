d7330040f6654a22e5f77951069983a03e54b4de
nparbs <nparbs@L11713042776.wctc.msft>
2016-02-23 15:10:07 -0600

    DiscountStrat - first commit

---
+++ src/njp/discountstrategy/Product.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njp.discountstrategy;
+
+/**
+ *
+ * @author nparbs
+ */
+public class Product {
+    
+}
