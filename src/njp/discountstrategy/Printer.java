366af66fa26e724541f7b34b10c01265a6d6a4b0
Nick <Nick@NParbsLaptop>
2016-03-01 14:07:27 -0600

    discount strat - validation

--- src/njp/discountstrategy/Printer.java
+++ src/njp/discountstrategy/Printer.java
@@ -20,15 +20,16 @@ public class Printer implements OutputStrategy{
         
         NumberFormat cur = NumberFormat.getCurrencyInstance(Locale.US);
         
+        
+        //Print to console
         System.out.println("Store: " + store + "        Customer Name: " 
                 + receipt.getCustomer().getCustName() + "          Date Time: " 
                 + LocalTime.now()+ "\n");
         System.out.println("Product Id       Product Name         Unit Price"
                 + "       Qty     Discount        Line Total");
          LineItem[] items = receipt.getLineItems(); 
-       for(LineItem i : items){ 
-        
-       System.out.println("       " + String.format("%-10s",i.getProduct().getProdId()) 
+        for(LineItem i : items){ 
+            System.out.println("       " + String.format("%-10s",i.getProduct().getProdId()) 
                + String.format("%-25s",i.getProduct().getProdName()) 
                + String.format("%-15s",cur.format(i.getProduct().getUnitCost())) 
                + String.format("%-8s",i.getQty()) 
---
+++ src/njp/discountstrategy/Validate.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njp.discountstrategy;
+
+/**
+ *
+ * @author Nick
+ */
+public class Validate {
+    
+    public final String checkString(String s){
+        if (s==null){
+            throw new IllegalArgumentException("Cannot be null");
+        } else if(s.length() >= 128) {
+            throw new IllegalArgumentException("String must be less than 128 chars");
+        } else {
+          return s;  
+        }
+    
+    }
+    
+}
